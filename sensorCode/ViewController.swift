33d86e58b9f71e1525074e22bd3f57f5699ae78b
Mark Lucking <mark.lucking@gmail.com>
2019-09-23 09:59:22 +0200

    Initial Commit

---
+++ sensorCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sensorCode
+//
+//  Created by localadmin on 23.09.19.
+//  Copyright © 2019 Mark Lucking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
